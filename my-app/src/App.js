d85ed68ab2dab8e1afb63bccfb42cf8c456145e9
SongtingY <sy642@scarletmail.rutgers.edu>
2023-10-14 11:54:10 -0700

    基本框架

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from "./pages/Index";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Project from "./pages/Projects";
+
+const { PUBLIC_URL } = process.env;
+
 
 function App() {
   return (
-    <div className="App">
-      hiii
-    </div>
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Routes>
+        <Route path='/' element={<Index/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/projects' element={<Project/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

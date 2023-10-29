a83fbda798261f5178a3df635f4f7337b2e21b15
SongtingY <sy642@scarletmail.rutgers.edu>
2023-10-29 14:14:34 -0700

    start some skills part
    - need to stylish the previous section
    - design skill section

---
+++ my-app/src/data/Resume/skills.js
@@ -0,0 +1,16 @@
+const skills = [
+    {
+        name: 'Programming Languages',
+        list: [
+            'JavaScript', 'Python', 'HTML', 'CSS', 'R', 'Java', 'PHP'
+        ]
+    },
+    {
+        name: 'Framework & Tools',
+        list: [
+            'MySQL', 'PostgreSQL', 'Git', 'React.js (Redux)', 'Bootstrap', 'Node.js', 'MongoDB', 'Spring Boot', 'NoSQL', 'Django'
+        ]
+    },
+]
+
+export default skills;
